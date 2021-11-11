eeab620dd1dfaef845e99c7cc69333cc03f5c127
YashiDev556 <yashidev556@gmail.com>
2021-11-10 19:17:05 -0500

    Uploaded Project from Computer

---
+++ src/WebScraper.java
@@ -0,0 +1,89 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+public class WebScraper {
+
+
+    //does not need a constructor yet...
+
+    public String zacksAnalystStatusGetter(String ticker)
+    {
+
+        ArrayList<String> ratings = new ArrayList<String>();
+
+        String rating = "";
+
+        try {
+
+            String url = "https://www.zacks.com/stock/quote/" + ticker + "?q=" + ticker;
+
+
+            final Document document = Jsoup.connect(url).get();
+
+            for(int i = 1; i <= 5; i++)
+                ratings.add(document.getElementsByClass("rank_chip rankrect_" + i).text());
+
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            ;
+        }
+
+        for(int j = 0; j < 5; j++)
+        {
+            if(ratings.size() != 0 && !ratings.get(j).equals(""))
+                rating = ratings.get(j);
+        }
+
+
+        return rating;
+    }
+
+    public double grabPrice(String ticker) throws IOException {
+
+        try {
+            String url = "https://finance.yahoo.com/quote/" + ticker + "?p=" + ticker;
+
+
+            final Document document = Jsoup.connect(url).get();
+
+            String price = document.getElementsByClass("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text();
+
+            price = price.replace(",", "");
+
+            return Double.parseDouble(price);
+
+        } catch (UnsupportedEncodingException | FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+            System.out.println("Couldn't get the link to: " + ticker);
+
+        }
+
+        return -1;
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,97 @@
+/**
+ * @author YashiDev556
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main (String[] args) throws IOException {
+
+
+
+        WebScraper webScraper = new WebScraper();
+
+        System.out.println("Zacks Stocks Webscraper      Author: YashiDev556 \n \n");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What rating stocks would you like to search for? (1-5, with 1 being strong buy and 5 being strong sell): ");
+        String desiredRating = input.nextLine();
+
+
+
+        System.out.println("Minimum Price? (Enter an Integer value): ");
+        int min = Integer.parseInt(input.nextLine());
+
+
+        System.out.println("Maximum Price? (Enter an Integer value): ");
+        int max = Integer.parseInt(input.nextLine());
+
+
+
+        System.out.println("Give us some time to search all of your desired Zacks stocks. Once we have them, you'll be good to go!");
+
+
+        // Open the file
+        FileInputStream fstream = new FileInputStream("StockTickers.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        //PrintStream out = new PrintStream(new FileOutputStream("GoodStocksAccordingToZacks.txt"));
+
+        StringBuilder loadingAnim = new StringBuilder(".");
+
+        File log = new File("StockApplicationOutput.txt");
+        try{
+            if(!log.exists()){
+                System.out.println("We had to make a new file.");
+                log.createNewFile();
+            }
+        }finally
+        {
+            ;
+        }
+
+
+
+        String ticker;
+
+        //Read File Line By Line
+        while ((ticker = br.readLine()) != null)   {
+            // Print the content on the console
+
+            System.out.print(loadingAnim);
+
+            //System.out.println(ticker); //delete this statement, it's for debugging purposes
+            if(webScraper.zacksAnalystStatusGetter(ticker).equals(desiredRating) && (webScraper.grabPrice(ticker) >= min) && (webScraper.grabPrice(ticker) <= max))
+            {
+                System.out.println(ticker);
+                try (PrintWriter out = new PrintWriter(new FileWriter("StockApplicationOutput.txt", true))) {
+                    out.append(ticker + "\n");
+                }
+            }
+
+
+
+
+
+        }
+
+
+        System.out.println("All done! Have a nice day! \n \n Github Profile Link: https://github.com/YashiDev556");
+        System.out.println("Press any Key to exit");
+        String quitter = input.nextLine();
+
+        //System.setOut(out);
+
+        //Close the input stream
+        fstream.close();
+
+
+
+
+
+    }
+
+
+}
+//https://finance.yahoo.com/chart/AMD?technical=short&ncid=dcm_306158732_490172245_127172993
